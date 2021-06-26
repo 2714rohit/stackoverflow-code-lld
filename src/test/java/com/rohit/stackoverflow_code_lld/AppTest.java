26117a61c0958426768257480a023f57505776f4
Rohit Kumar Maurya <2714rohit@gmail.com>
2021-06-27 02:49:05 +0530

    initial commit

---
+++ src/test/java/com/rohit/stackoverflow_code_lld/AppTest.java
@@ -0,0 +1,20 @@
+package com.rohit.stackoverflow_code_lld;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
